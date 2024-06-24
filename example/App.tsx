a72718e1bd4f958d58f977b9d47aaaffdc9e1ec7
Evo <evo@MacBook-Air-de-Evo.local>
2024-06-24 12:53:55 -0300

    Relative time
    Signed-off-by: Evo <evo@MacBook-Air-de-Evo.local>

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,21 @@
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 import * as ExpoRelativeTime from 'expo-relative-time';
 
 export default function App() {
+  const [time, setTime] = React.useState(0);
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setTime(ExpoRelativeTime.getRelativeTime())
+    }, 1000)
+    setTime(ExpoRelativeTime.getRelativeTime())
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>{ExpoRelativeTime.hello()}</Text>
+      <Text>{time}</Text>
     </View>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to ExpoRelativeTime.web.ts
-// and on native platforms to ExpoRelativeTime.ts
 import ExpoRelativeTimeModule from './ExpoRelativeTimeModule';
-import ExpoRelativeTimeView from './ExpoRelativeTimeView';
-import { ChangeEventPayload, ExpoRelativeTimeViewProps } from './ExpoRelativeTime.types';
 
-// Get the native constant value.
-export const PI = ExpoRelativeTimeModule.PI;
-
-export function hello(): string {
-  return ExpoRelativeTimeModule.hello();
+export function getRelativeTime(): number {
+  return ExpoRelativeTimeModule.getRelativeTime();
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoRelativeTimeModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoRelativeTimeModule ?? NativeModulesProxy.ExpoRelativeTime);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoRelativeTimeView, ExpoRelativeTimeViewProps, ChangeEventPayload };
